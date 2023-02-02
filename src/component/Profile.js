9a0d023f32f44744276031dba4c0921ebd9f3723
Sébastien Ternest <sebt-max@MacBook-Pro-de-Sebastien-Ternest.local>
2023-02-03 00:08:39 +0100

    second commit

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -2,7 +2,7 @@ import { getSuggestedQuery } from '@testing-library/react';
 import React,{Fragment} from 'react';
 import imgseb from '../me.png';
 import selftitle from '../sebTitle.png';
-import { FaBeer } from "react-icons/fa";
+import { FaBeer, FaRegIdBadge } from "react-icons/fa";
 import { BiCurrentLocation } from "react-icons/bi";
 import { BsFillPhoneFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
@@ -18,22 +18,41 @@ const Profile = () => {
                     <h1 style={{color:'#FFF'}}>Sébastien Ternest</h1> 
                     <h2 className="jobTitle">FullStack Developer</h2>
                 <div className="profile-data" >            
-                    <h3 style={{color:'#FFF'}}>Phone</h3>
-                    <span style ={{color: "white", background:"red",fontSize:"1.5em"}}>
-                    <BsFillPhoneFill /> 
-                    </span>   
+                   <div className='flex'>
+                        <span style ={{marginRight:15,color:"lightblue",fontSize:"2.5em"}}>
+                        <BsFillPhoneFill /> 
+                        </span> 
+                        
+                        <div>
+                        <h6 style={{color:'grey'}}>Phone</h6>
                         <p>+32468129890</p>
-                    <h3 style={{color:'#FFF'}}>Location</h3>
-                    <span style ={{color: "white", fontSize:"1.5em"}}>
-                    <BiCurrentLocation /> 
-                    </span>
-                        <p>Rue Notre-Dame,21</p> 
-                        <p>1420 Braine-l'Alleud</p>
-                    <h3 style={{color:'#FFF'}}>Email</h3>
-                    <span style ={{color: "white", fontSize:"1.5em"}}>
-                    <MdEmail /> 
-                    </span>
-                        <p>sebternest@hotmail.com</p>                          
+                        </div>  
+                        
+                    </div>
+                    
+
+                    <div className='flex'>
+                        <span style ={{marginRight:15,color: "lightcoral",fontSize:"2.5em"}}>
+                        <BiCurrentLocation /> 
+                        </span> 
+                        
+                        <div>
+                        <h6 style={{color:'grey'}}>Location</h6>
+                        <p>Braine-l'Alleud</p>
+                        </div>  
+                        
+                    </div>
+                    <div className='flex'>
+                        <span style ={{marginRight:15,color: "lightgray",fontSize:"2.5em"}}>
+                        <MdEmail /> 
+                        </span> 
+                        
+                        <div>
+                        <h6 style={{color:'grey'}}>Phone</h6>
+                        <p>sebternest@hotmail.com</p>
+                        </div>  
+                        
+                    </div>                          
                 </div>
                 <div className="downloadCV">Download CV</div>
             </div> 
